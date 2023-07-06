37f2b522b2bde744f00d1da24f41030ebb7ff5f2
AntonelloStefania <antonelloste94@gmail.com>
2023-07-06 13:30:53 +0200

    collegati fogli di lavoro

--- index.php
+++ index.php
@@ -10,8 +10,18 @@
         <title>ToDoList</title>
     </head>
     <body>
-        <div id="app">
-
+        <div class="wrapper">
+            <div id="app">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <ul>
+                                <li>{{hello}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <script type="text/javascript" src="./js/script.js"></script>
     </body>
---
+++ server.php
@@ -0,0 +1,4 @@
+<?php
+$string = file_get_contents('data/to_do.json');
+echo $string
+?>
